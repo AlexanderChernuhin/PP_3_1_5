6ae0fda9b17dd16ba78cffffae8088fd4da8a807
AlexanderChernuhin <alexander_963@bk.ru>
2024-05-28 23:27:07 +0300

    Написать Rest-контроллеры для вашего приложения

---
+++ src/main/java/ru/kata/spring/boot_security/demo/model/Role.java
@@ -0,0 +1,62 @@
+package ru.kata.spring.boot_security.demo.model;
+
+
+import org.springframework.security.core.GrantedAuthority;
+import java.util.List;
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(unique = true)
+    private String role;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+    public Role() {
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return role;
+    }
+
+    @Override
+    public String toString() {
+        return role.replace("ROLE_", "");
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
